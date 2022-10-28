53a5e68d94813b404dcef32bf7ea4eb47167e8dc
khadkabobby <108467070+khadkabobby@users.noreply.github.com>
2022-10-28 16:36:56 +0545

    uploading the files for university projects

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("welcome");
+
+function menuToggle() {
+  let menuBar = document.getElementById("menuBar");
+  menuBar.style.right = "0px";
+  menuBar.style.transition = "0.5s";
+}
+function offMenu() {
+  let menuBar = document.getElementById("menuBar");
+  menuBar.style.right = "-200px";
+  menuBar.style.transition = "0.5s";
+  l;
+}
